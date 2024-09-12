199c86e9c1740f8463146c8ac40af42dafb6dca8
Sopheaktraleng <sopheaktraleng@Sopheaktras-MacBook-Pro.local>
2024-09-12 11:38:32 +0700

    layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,16 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="continer">
-    <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Student Management</a>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+  <div class="continer">
+    <!-- Header -->
+      @include("includes.header")
+    <!-- Header -->
+    <!-- Sidebar -->
+      @include("includes.sidebar")
+    <!-- Sidebar -->
   </div>
-</nav>
-    </div>
 </body>
 </html>
